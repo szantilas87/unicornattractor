68e52ff0b46ac7c7d2c09c083cf0333fcd01d386
szantilas87 <szantilas@gmail.com>
2019-08-29 08:55:38 +0100

    Changes in forntend

--- unicorn/settings.py
+++ unicorn/settings.py
@@ -84,17 +84,17 @@ WSGI_APPLICATION = 'unicorn.wsgi.application'
 
 #Database setting for Heroku 
 
-# DATABASES = {
-#     'default': dj_database_url.config()
-# }
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 #Database setting for development
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
